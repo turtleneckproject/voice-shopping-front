64d2889082e21437de7e92803c238a9d18e1f4f6
jinwoo <bluekjw3516@naver.com>
2021-10-05 15:58:09 +0900

    start

---
+++ voca/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './component/Hello'
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="App">
+    <Hello />
+    <div className={styles.box}>App</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ voca/src/component/Hello.js
@@ -0,0 +1,29 @@
+export default function Hello() {
+    function showName() {
+        console.log("Mike");
+    }
+    function showAge(age) {
+        console.log(age);
+    }
+    function showText(txt) {
+        console.log(txt);
+    }
+
+    return (
+        <div>
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button
+                onClick={() => {
+                    showAge(10);
+                }}
+            >
+                Show age
+            </button>
+            <input type="text" onChange={e => {
+                const txt = e.target.value;
+                showText(txt);
+            }}/>
+        </div>
+    );
+}
